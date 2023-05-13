98620f8cebd16e135b3f1a69993b7e4a9cfef6a4
ejImDev <ejim0093@gmail.com>
2023-05-14 03:02:59 +0900

    Binary Min Heap

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,4 +1,6 @@
 import AVL.AVL;
+import BinaryMinHip.BHeap;
+import BinaryMinHip.Entry;
 import List.*;
 import SerchTree.BST;
 import Tree.BinaryTree;
@@ -222,6 +224,7 @@ public class Main {
         */
 
         //AVL트리
+        /*
         AVL<Integer, String> st = new AVL<Integer, String>();
         st.put(75, "Apple");
         st.put(80, "Grape");
@@ -240,6 +243,35 @@ public class Main {
         System.out.printf("75와 85 삭제 후 : \n");
         st.delete(75);
         st.delete(85);
-        st.print(st.root);
+        st.print(st.root);*/
+
+        //이진 힙
+        Entry[] a = new Entry[16];
+        a[1] = new Entry(90, "watermelon");
+        a[2] = new Entry(80, "pear");
+        a[3] = new Entry(70, "melon");
+        a[4] = new Entry(50, "lime");
+        a[5] = new Entry(60, "mango");
+        a[6] = new Entry(20, "cherry");
+        a[7] = new Entry(30, "grape");
+        a[8] = new Entry(35, "orange");
+        a[9] = new Entry(10, "apricot");
+        a[10] = new Entry(15, "banana");
+        a[11] = new Entry(45, "lemon");
+        a[12] = new Entry(40, "kiwi");
+
+        BHeap h = new BHeap(a,12);
+        System.out.println("힙 만들기 전 : ");
+        h.print();
+        h.createHeap();
+        System.out.println("최소 힙 : ");
+        h.print();
+        System.out.println("min 삭제 후 : ");
+        System.out.println(h.deleteMin().getValue());
+        h.print();
+        h.insert(5,"apple");
+        System.out.println("5 삽입 후 : ");
+        h.print();
+
     }
 }
